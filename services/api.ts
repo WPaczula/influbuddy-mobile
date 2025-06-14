6a1a4d042ef29cf0c0f9c4800946e4ac8d63c968
Wojciech Paczuła <wojciech.paczula@gmail.com>
2025-06-14 23:38:50 +0200

    Updated api.ts

--- services/api.ts
+++ services/api.ts
@@ -6,7 +6,7 @@ import { Platform } from 'react-native';
 const API_URL = Platform.select({
   ios: 'http://localhost:3000/api/v1',
   android: 'http://10.0.2.2:3000/api/v1',
-  default: 'http://localhost:3000/api/v1',
+  default: 'https://888b-193-25-251-171.ngrok-free.app/api/v1',
 });
 
 export const api = axios.create({
